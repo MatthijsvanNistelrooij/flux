6d27c7f679727cc677d2ed83852798f2f2809ea1
Matthijs <matthijs.vannistelrooij@gmail.com>
2024-12-08 03:29:52 +0100

    updated sidebar links

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,16 +1,20 @@
 "use client"
+import { cn } from "@/lib/utils"
+
 import Link from "next/link"
 import React, { useEffect, useState } from "react"
 import LoginButton from "./LoginButton"
 import LogoutButton from "./LogoutButton"
-import { useRouter } from "next/navigation"
+import { usePathname, useRouter } from "next/navigation"
 import { Account, Client } from "appwrite"
 import { User } from "@/types"
 import { Button } from "./ui/button"
+import { sidebarLinks } from "@/constants"
 
 const Sidebar = () => {
   const [user, setUser] = useState<User | null>(null)
   const [userId, setUserId] = useState<string | null>(null)
+  const pathname = usePathname()
 
   const router = useRouter()
 
@@ -36,19 +40,27 @@ const Sidebar = () => {
 
   return (
     <nav className="sidebar">
-      <div className="flex flex-col gap-2 p-2">
-        <Link href={"/"} className="nav-link">
-          Gallery
-        </Link>
-        <Link href={"/create"} className="nav-link">
-          Create
-        </Link>
-        <Link href={"/profile"} className="nav-link">
-          Profile
-        </Link>
-        <Link href={"/tokens"} className="nav-link">
-          Tokens
-        </Link>
+      <div className="flex flex-col gap-2 mt-4">
+        {sidebarLinks.map(({ route, label, imgURL }) => {
+          const isActive =
+            pathname === route || pathname.startsWith(`${route}/`)
+
+          return (
+            <Link
+              href={route}
+              key={label}
+              className={cn(
+                "flex gap-3 items-center py-4 max-lg:px-4 justify-start lg:justify-start hover:border-r-4 border-orange-400 pl-4 hover:bg-gradient-to-r from-gray-50 to-slate-300",
+                {
+                  "bg-nav-focus border-r-4 border-orange-400 bg-gradient-to-r from-slate-50 to-slate-300":
+                    isActive,
+                }
+              )}
+            >
+              <p className="font-semibold text-gray-800">{label}</p>
+            </Link>
+          )
+        })}
       </div>
       <div>
         {user && (
---
+++ constants/index.ts
@@ -0,0 +1,23 @@
+export const sidebarLinks = [
+    {
+      imgURL: "/icons/home.svg",
+      route: "/",
+      label: "Gallery",
+    },
+    {
+      imgURL: "/icons/edit.svg",
+      route: "/create",
+      label: "Create",
+    },
+    {
+      imgURL: "/icons/profile.svg",
+      route: "/profile",
+      label: "Profile",
+  },
+  {
+    imgURL: "/icons/profile.svg",
+    route: "/tokens",
+    label: "Tokens",
+  },
+  ];
+  
