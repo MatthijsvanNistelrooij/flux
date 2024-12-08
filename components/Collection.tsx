a7ebc3e4d2ff2993637e1db6b20b7286a8e5f1ed
Matthijs <matthijs.vannistelrooij@gmail.com>
2024-12-08 06:18:34 +0100

    added contact

--- components/Collection.tsx
+++ components/Collection.tsx
@@ -121,7 +121,7 @@ const Collection = ({ filter }: CollectionProps) => {
       )}
 
       <div
-        className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 ${
+        className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-16 lg:mt-0 ${
           filter === "create" ? "opacity-5" : ""
         }`}
       >
