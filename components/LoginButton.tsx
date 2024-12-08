eea5d3a8b5a126412d4cbbd02082227cc4a7405e
Matthijs <matthijs.vannistelrooij@gmail.com>
2024-12-08 06:46:07 +0100

    updated buttons added redirect

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -11,7 +11,7 @@ const LoginButton = () => {
       console.log("Starting Google login process...")
       await account.createOAuth2Session(
         "google" as OAuthProvider,
-        "https://flux-sage.vercel.app/",
+        "https://flux-sage.vercel.app/profile",
         "https://flux-sage.vercel.app/fail"
       )
     } catch (err) {
